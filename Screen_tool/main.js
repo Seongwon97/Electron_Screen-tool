b8edf0d062f440774cc9b6a5e109eb4ee6017e12
Seongwon97 <osw970325@naver.com>
2021-01-07 23:37:12 +0900

    Tutorial-Hello World
    Unable to execute with error "ReferenceError: requere is not defined".
    It must modify.

---
+++ Screen_tool/main.js
@@ -0,0 +1,45 @@
+console.log("Hello world");
+
+const electron = requere("electron");
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const path = require("path");
+const url = require("url");
+
+let win;
+
+function createWindow(){
+    win = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file',
+        slashes: true
+    }));  // 'index.html' 파일을 이용하여 browser window생성
+
+    //win.webContents.openDevTools();
+    //사용자 툴이 나오도록 하는 코드
+
+    win.on('closed', () => {
+        win = null;
+    }); //사용자가 윈도우를 닫을 수 있도록 설정
+} 
+
+app.on('ready', createWindow);
+//initialize 작업이 완료되면 createWindow를 실행.
+
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow();
+  }
+});
