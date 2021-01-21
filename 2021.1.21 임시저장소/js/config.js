dd206ec0cc3aa20fdd870bc74d3b2f38416fed9a
Seongwon97 <57744251+Seongwon97@users.noreply.github.com>
2021-01-21 17:47:15 +0900

    Modify login, signup, password_recover page and create layout for main page

---
+++ 2021.1.21 임시저장소/js/config.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAYIi_l0DC3zhLwxOxXHWOGTW9LD6IwFn0",
+    authDomain: "cloudike-feedback-5309e.firebaseapp.com",
+    projectId: "cloudike-feedback-5309e",
+    storageBucket: "cloudike-feedback-5309e.appspot.com",
+    messagingSenderId: "315063549228",
+    appId: "1:315063549228:web:323c00bb29af9b43218089"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
