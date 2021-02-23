07f6a9a069a8b6a325fe228c5895fd54840da736
Seongwon97 <57744251+Seongwon97@users.noreply.github.com>
2021-02-23 17:15:49 +0900

    Update editing.js

--- CloudWhisper/js/editing.js
+++ CloudWhisper/js/editing.js
@@ -1,4 +1,3 @@
-var project_name;
 var lastEvent;
 var isMouseDown = false;
 var line=false;
@@ -27,11 +26,17 @@ var x, y, scale; //canvas에서 사진의 위치 및 크기 조정을 위한 변
 var line_color; //line의 색과 넓이를 담을 변수
 var line_width = 5;
 
+var first_read = true;
 var annotation =[]; //firebase에서 annotation의 값을 받아와서 저장할 array
 var count = 0; //annotation의 개수를 카운트
 var del_count = 0; //삭제된 annotation의 개수
 var comment_content = ""; //comment의 내용을 담을 변수
 var user_name = "유저"; // firebase에서 값 받아와서 저장
+var project_name = "Project A";
+var image_name = "Image A";
+var annoRef; //파이어베이스에 저장될 주소가 저장되는 변수
+var key = []; //annotation key를 저장하는 변수
+
 
 var windowHeight = 0;
 var img;
@@ -50,6 +55,7 @@ window.onload = function(){
             snapshot.forEach(function (data) {
                 if(data.val().email == firebase.auth().currentUser.email) {
                     user_name = data.val().name;
+                    console.log(user_name);
                     document.getElementById("setting_user_name").innerHTML = data.val().name;
                     document.getElementById("email").innerHTML = data.val().email;
                     document.getElementById("company_name").innerHTML = data.val().companyName;
@@ -58,7 +64,7 @@ window.onload = function(){
                         //이름이 3글자일 경우만 생각했다. 영어 이름이나 4글자부터는 다시 지정해야함.
                     }
                     else {
-                        document.getElementById("user_icon").innerHTML = data.val().name
+                        document.getElementById("user_icon").innerHTML = data.val().name;
                     }
                 }
             });
@@ -71,6 +77,21 @@ window.onload = function(){
 
     //전 페이지에서 선택된 projcet의 이름을 받아오기
     //저장은 project_name변수에
+    var project_data_address = "Project/".concat(project_name+"/"+image_name+"/annotation/");
+    annoRef = firebase.database().ref(project_data_address);
+
+    annoRef.on('child_added', function(data) {
+        key.push(data.key);
+        if(first_read) {
+            annotation.push(data.val());
+            add_comment_canvas(count);
+            add_comment_list(count);
+            add_annotation_info(count);
+            count++;
+            draw_annotation();
+        }
+    })
+
 
     //window load시 오른쪽에 위치한 context 높이 조정
     windowHeight = window.innerHeight;
@@ -114,6 +135,7 @@ window.onload = function(){
 
     canvas.addEventListener("mousedown", function(e){
         //모든 annotation이 컨펌되어서 그릴수 있는 상태면 측정 시작
+        first_read = false;
         if (possible) {
             lastEvent = e;
             isMouseDown = true;
@@ -207,7 +229,7 @@ window.onload = function(){
             screen_sy=(screen_sy-168-y)/(img.height * scale);
             screen_ex=(screen_ex-159-x)/(img.width * scale);
             screen_ey=(screen_ey-168-y)/(img.height * scale);
-    
+
             if (line) {
                 if (!((Math.abs(sx-ex)<10)&&(Math.abs(sy-ey)<10))) {
                     sx=(sx-x)/(img.width * scale);
@@ -217,18 +239,18 @@ window.onload = function(){
     
                     annotation.push({
                         project: project_name,
-                        num : count,
+                        image: image_name,
+                        user: user_name,
+                        type: 'line',
+                        comment: comment_content,
+                        date: new Date().toLocaleString(),
                         start_x: sx,
                         start_y: sy,
                         end_x: ex,
                         end_y: ey,
-                        type: 'line',
                         color: line_color,
                         lineWidth: line_width,
                         remain: true,
-                        user: user_name,
-                        comment: comment_content,
-                        date: new Date().toLocaleString(),
                         clicked: false,
                         screen_sx: screen_sx,
                         screen_sy: screen_sy,
@@ -238,6 +260,7 @@ window.onload = function(){
                     });
                     add_comment_canvas(count);
                     add_annotation_info(count);
+
                     count++;
                 }
             }
@@ -250,18 +273,18 @@ window.onload = function(){
     
                     annotation.push({
                         project: project_name,
-                        num : count,
+                        image: image_name,
+                        user: user_name,
+                        type: 'square',
+                        comment: comment_content,
+                        date: new Date().toLocaleString(),
                         start_x: sx,
                         start_y: sy,
                         end_x: ex,
                         end_y: ey,
-                        type: 'square',
                         color: line_color,
                         lineWidth: line_width,
                         remain: true,
-                        user: user_name,
-                        comment: comment_content,
-                        date: new Date().toLocaleString(),
                         clicked: false,
                         screen_sx: screen_sx,
                         screen_sy: screen_sy,
@@ -282,18 +305,18 @@ window.onload = function(){
                     ex = cir_radius /(img.width * scale);
                     annotation.push({
                         project: project_name,
-                        num : count,
+                        image: image_name,
+                        user: user_name,
+                        type: 'circle',
+                        comment: comment_content,
+                        date: new Date().toLocaleString(),
                         start_x: sx,
                         start_y: sy,
                         end_x: ex,
                         end_y: null,
-                        type: 'circle',
                         color: line_color,
                         lineWidth: line_width,
                         remain: true,
-                        user: user_name,
-                        comment: comment_content,
-                        date: new Date().toLocaleString(),
                         clicked: false,
                         screen_sx: screen_sx,
                         screen_sy: screen_sy,
@@ -315,18 +338,18 @@ window.onload = function(){
                     ey=(ey-y)/(img.height * scale);
                     annotation.push({
                         project: project_name,
-                        num : count,
+                        image: image_name,
+                        user: user_name,
+                        type: 'arrow',
+                        comment: comment_content,
+                        date: new Date().toLocaleString(),
                         start_x: sx,
                         start_y: sy,
                         end_x: ex,
                         end_y: ey,
-                        type: 'arrow',
                         color: line_color,
                         lineWidth: line_width,
                         remain: true,
-                        user: user_name,
-                        comment: comment_content,
-                        date: new Date().toLocaleString(),
                         clicked: false,
                         screen_sx: screen_sx,
                         screen_sy: screen_sy,
@@ -344,18 +367,18 @@ window.onload = function(){
                 sy=(sy-y)/(img.height * scale);
                 annotation.push({
                     project: project_name,
-                    num : count,
+                    image: image_name,
+                    user: user_name,
+                    type: 'comment',
+                    comment: comment_content,
+                    date: new Date().toLocaleString(),
                     start_x: sx,
                     start_y: sy,
                     end_x: null,
                     end_y: null,
-                    type: 'comment',
                     color: line_color,
                     lineWidth: line_width,
                     remain: true,
-                    user: user_name,
-                    comment: comment_content,
-                    date: new Date().toLocaleString(),
                     clicked: false,
                     screen_sx: screen_sx,
                     screen_sy: screen_sy,
@@ -951,7 +974,6 @@ function add_comment_list(order) {
     var list_date_id = "annotation_list_date".concat(order);
     var list_comment_id = "annotation_list_comment".concat(order);
     var comment_list_div_id = "annotation_list".concat(order);
-    //div.innerHTML = "Num: " + annotation[order].num +"<br/>User name: " + annotation[order].user_name + "<br/>type: " + annotation[order].type + "<br/>Date: "+ annotation[order].date;
     comment_list_div.id = comment_list_div_id;
     if (annotation[order].clicked) {
         comment_list_div.style.border = "2px solid red";
@@ -999,14 +1021,16 @@ function add_comment_list(order) {
             parent.removeChild(comment_canvas_div);
 
             annotation[order].remain=false;
+            annoRef.child(key[order]).remove();
 
             toDelete = true;
             del_count++;
-            console.log("Deleted the", annotation[order].num, "th annotation the ",annotation[order].type);
+            console.log("Deleted the", order, "th annotation (",annotation[order].type,")");
             console.log("Number of annotation: ", count-del_count);
             //이곳에 firebase에서의 데이터 삭제 내용도 추가할 것.
             delete_annotation_info (order)
             draw_annotation();
+            
         }else{
             toDelete = true;
             return;
@@ -1028,7 +1052,6 @@ function add_comment_canvas(order) {
     var canvas_comment_id = "annotation_canvas_comment".concat(order);
     var canvas_confirm_id = "annotation_canvas_confirm".concat(order);
     var canvas_revise_id = "annotation_canvas_revise".concat(order);
-    //div.innerHTML = "Num: " + annotation[order].num +"<br/>User name: " + annotation[order].user_name + "<br/>type: " + annotation[order].type + "<br/>Date: "+ annotation[order].date;
     comment_div.id = comment_div_id;
 
     screen_ex = annotation[order].screen_ex * scale * img.width + x;
@@ -1083,10 +1106,11 @@ function add_comment_canvas(order) {
                 parent.removeChild(comment_list_div);
                                 
                 annotation[order].remain=false;
- 
+                annoRef.child(key[order]).remove();
+                
                 toDelete = true;
                 del_count++;
-                console.log("Deleted the", annotation[order].num, "th annotation the ",annotation[order].type);
+                console.log("Deleted the", order, "th annotation (",annotation[order].type,")");
                 console.log("Number of annotation: ", count-del_count);
                 //이곳에 firebase에서의 데이터 삭제 내용도 추가할 것.
                 delete_annotation_info (order);
@@ -1145,11 +1169,14 @@ function add_comment_canvas(order) {
         comment.innerHTML=textarea_div.value;
         comment_div.appendChild(comment);
         if(annotation[order].new) {
+            annotation[order].new = false;
+            annoRef.push(annotation[order]);
+
             if(comment_list){
                 annotation[order].clicked = true;
                 add_comment_list(order);
             }
-            annotation[order].new = false;
+        
             console.log("Number of annotation: ", count-del_count);
         }
         else {
@@ -1157,6 +1184,7 @@ function add_comment_canvas(order) {
             var list_comment_id = "annotation_list_comment".concat(order);
             document.getElementById(list_date_id).innerHTML = annotation[order].date;
             document.getElementById(list_comment_id).innerHTML = annotation[order].comment;
+            revise_firebase(order);
         }
         
         possible = true;
@@ -1185,7 +1213,7 @@ function add_comment_canvas(order) {
 
 
     if (!annotation[order].new) {
-        if (annotation[order].num != isClicked) {
+        if (order != isClicked) {
             $("#".concat(comment_div_id)).hide();
         }
     }
@@ -1235,7 +1263,13 @@ function add_annotation_info (order) {
     annotation_info.style.top = screen_sy.toString().concat("px");
     annotation_info.style.left = screen_sx.toString().concat("px");
     annotation_info.style.backgroundColor = annotation[order].color;
-    annotation_info.innerHTML = annotation[order].user;
+    if (annotation[order].user.length >= 2) {
+        annotation_info.innerHTML = annotation[order].user.substring(1, 3);
+    }
+    else {
+        annotation_info.innerHTML = annotation[order].user;
+    }
+    
     document.getElementById('editing_canvas').appendChild(annotation_info);
 
     annotation_info.onclick = function() {
@@ -1296,3 +1330,13 @@ function annotation_click_event(order) {
     }
     draw_annotation();
 }
+
+
+function revise_firebase(order) {
+    annoRef.child(key[order]).update({
+        date: annotation[order].date,
+        comment: annotation[order].comment
+    });
+}
+
+
